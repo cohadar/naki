d267660b0da7d6bfd3bba628dc973be7c169a3f9
Damir Čohadarević <cohadar@gmail.com>
2020-04-21 22:06:12 +0200

    шк

--- код/наки/__main__.py
+++ код/наки/__main__.py
@@ -13,29 +13,11 @@ import dependency_injector.containers as containers
 import dependency_injector.providers as providers
 
 
-class Контејнер(containers.DynamicContainer):
-    def листа_дирова(к, каталог):
-        return [фајл for фајл in каталог.iterdir() if фајл.is_dir() and not фајл.name.startswith('__')]
-
-    def листа_шпилова(к):
-        рез = []
-        for дир in к.дирови():
-            к.дир.override(providers.Object(дир))
-            рез.append(к.шпил())
-        return рез
-
-    def листа_вежбања(к):
-        рез = []
-        for шпил in к.шпилови():
-            рез.append(к.вежбање(шпил=шпил))
-        return рез
-
-    def __init__(к):
+class ШпилКонтејнер(containers.DynamicContainer):
+    def __init__(к, дир):
         super().__init__()
-        к.каталог = providers.Object(ПУТАЊА_КАТАЛОГА)
-        к.дирови = providers.Callable(к.листа_дирова, каталог=к.каталог)
+        к.дир = providers.Object(дир)
 
-        к.дир = providers.Object(None)
         к.путања_интервали = providers.Callable(lambda дир: дир.joinpath('интервали.tsv'), к.дир)
         к.путања_карте = providers.Callable(lambda дир: дир.joinpath('карте.tsv'), к.дир)
         к.путања_линкови = providers.Callable(lambda дир: дир.joinpath('линкови.tsv'), к.дир)
@@ -58,11 +40,28 @@ class Контејнер(containers.DynamicContainer):
             п_карти=к.п_карти,
             п_линкова=к.п_линкова,
             п_записа=к.п_записа)
-        к.шпилови = providers.Callable(к.листа_шпилова)
+
+
+class Контејнер(containers.DynamicContainer):
+    def листа_дирова(к, каталог):
+        return [фајл for фајл in каталог.iterdir() if фајл.is_dir() and not фајл.name.startswith('__')]
+
+    def листа_шпил_контејнера(к):
+        return [к.шпил_контејнер(дир=дир) for дир in к.дирови()]
+
+    def листа_вежбања(к):
+        return [к.вежбање(шпил=шк.шпил()) for шк in к.шпил_контејнери()]
+
+    def __init__(к):
+        super().__init__()
+        к.каталог = providers.Object(ПУТАЊА_КАТАЛОГА)
+        к.дирови = providers.Callable(к.листа_дирова, каталог=к.каталог)
+        к.шпил_контејнер = providers.Factory(ШпилКонтејнер)
+        к.шпил_контејнери = providers.Callable(к.листа_шпил_контејнера)
 
         к.терминал = providers.Factory(Терминал)
         к.ui = providers.Singleton(UI, терминал=к.терминал)
-        к.вежбање = providers.Factory(Вежбање, ui=к.ui, путања_каталога=к.каталог, шпил=к.шпил)
+        к.вежбање = providers.Factory(Вежбање, ui=к.ui, путања_каталога=к.каталог)
         к.вежбања = providers.Callable(к.листа_вежбања)
         к.главна = providers.Factory(Главна, ui=к.ui, вежбања=к.вежбања)
 
